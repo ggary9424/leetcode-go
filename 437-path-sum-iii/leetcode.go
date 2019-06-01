ce3227e6ae2a389a371138c5c1b48944b0c0e5d6
Gary Chen <gary@unh3o.com>
2019-06-02 02:39:01 +0800

    Use thread pool

--- 437-path-sum-iii/leetcode.go
+++ 437-path-sum-iii/leetcode.go
@@ -1,56 +1,86 @@
 package leetcode_go
 
+import (
+	"sync"
+)
+
 type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type TreeNodeJob struct {
+	Callback func(node *TreeNode)
+	Node     *TreeNode
 }
 
 func pathSum(root *TreeNode, sum int) int {
-    if root == nil {
-        return 0
-    }
+	if root == nil {
+		return 0
+	}
 
-    c := 0
-    cChan := make(chan int)
-    nodeCount := traverse(root, func (node *TreeNode) {
-        cChan <- calculate(node, sum)
-    })
+	var mutex sync.Mutex
+	result := 0
+	jobChan := startTraverseWorker(10)
+	jobProcess := 0
+	nodeCount := traverse(jobChan, root, func(node *TreeNode) {
+		mutex.Lock()
+		result += calculate(node, sum)
+		jobProcess += 1
+		mutex.Unlock()
+	})
 
-    for i:=0;i<nodeCount;i++ {
-        c += <-cChan
-    }
-
-    return c
+	for jobProcess != nodeCount {
+		// do nothing
+	}
+	return result
 }
 
 func calculate(root *TreeNode, sum int) int {
-    v := sum - root.Val
-    c1, c2 := 0, 0
-    if root.Left != nil {
-        c1 = calculate(root.Left, v)
-    }
-    if root.Right != nil {
-        c2 = calculate(root.Right, v)
-    }
-    if v == 0 {
-        return 1 + c1 + c2
-    }
-    return c1 + c2
+	v := sum - root.Val
+	c1, c2 := 0, 0
+	if root.Left != nil {
+		c1 = calculate(root.Left, v)
+	}
+	if root.Right != nil {
+		c2 = calculate(root.Right, v)
+	}
+	if v == 0 {
+		return 1 + c1 + c2
+	}
+	return c1 + c2
 }
 
-func traverse(node *TreeNode, callback func (*TreeNode)) int {
-    c1, c2 := 0, 0
-    if node == nil {
-        return 0
-    }
+func startTraverseWorker(number int) chan TreeNodeJob {
+	treeNodeJobs := make(chan TreeNodeJob, 10*number)
+	for i := 0; i < number; i++ {
+		go func() {
+			for true {
+				j := <-treeNodeJobs
+				j.Callback(j.Node)
+			}
+		}()
+	}
+	return treeNodeJobs
+}
 
-    go callback(node)
-    if node.Left != nil {
-        c1 = traverse(node.Left, callback)
-    }
-    if node.Right != nil {
-        c2 = traverse(node.Right, callback)
-    }
-    return 1 + c1 + c2
+func traverse(jobChan chan TreeNodeJob, node *TreeNode, callback func(*TreeNode)) int {
+	c1, c2 := 0, 0
+	if node == nil {
+		return 0
+	}
+
+	jobChan <- TreeNodeJob{
+		Callback: callback,
+		Node:     node,
+	}
+
+	if node.Left != nil {
+		c1 = traverse(jobChan, node.Left, callback)
+	}
+	if node.Right != nil {
+		c2 = traverse(jobChan, node.Right, callback)
+	}
+	return 1 + c1 + c2
 }
--- 437-path-sum-iii/leetcode_test.go
+++ 437-path-sum-iii/leetcode_test.go
@@ -1,50 +1,369 @@
 package leetcode_go
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestPathSum(t *testing.T) {
-    tree := &TreeNode{
-        Val: 10,
-        Left: &TreeNode{
-            Val: 5,
-            Left: &TreeNode{
-                Val: 3,
-                Left: &TreeNode{
-                    Val: 3,
-                    Left: nil,
-                    Right: nil,
-                },
-                Right: &TreeNode{
-                    Val: -2,
-                    Left: nil,
-                    Right: nil,
-                },
-            },
-            Right: &TreeNode{
-                Val: 2,
-                Left: nil,
-                Right: &TreeNode{
-                    Val: 1,
-                    Left: nil,
-                    Right: nil,
-                },
-            },
-        },
-        Right: &TreeNode{
-            Val: -3,
-            Left: nil,
-            Right: &TreeNode{
-                Val: 11,
-                Left: nil,
-                Right: nil,
-            },
-        },
-    }
-    
-    result := pathSum(tree, 8)
-    if result != 3 {
-        t.Errorf("The result should be 3, but we got %d", result)
-    }
+	tree := &TreeNode{
+		Val: 10,
+		Left: &TreeNode{
+			Val: 5,
+			Left: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val: -2,
+					Left: &TreeNode{
+						Val: 3,
+						Left: &TreeNode{
+							Val: 3,
+							Left: &TreeNode{
+								Val: 3,
+								Left: &TreeNode{
+									Val:   3,
+									Left:  nil,
+									Right: nil,
+								},
+								Right: &TreeNode{
+									Val:  -2,
+									Left: nil,
+									Right: &TreeNode{
+										Val: 3,
+										Left: &TreeNode{
+											Val:  3,
+											Left: nil,
+											Right: &TreeNode{
+												Val: 3,
+												Left: &TreeNode{
+													Val:   3,
+													Left:  nil,
+													Right: nil,
+												},
+												Right: &TreeNode{
+													Val:   -2,
+													Left:  nil,
+													Right: nil,
+												},
+											},
+										},
+										Right: &TreeNode{
+											Val:   -2,
+											Left:  nil,
+											Right: nil,
+										},
+									},
+								},
+							},
+							Right: nil,
+						},
+						Right: &TreeNode{
+							Val:  -2,
+							Left: nil,
+							Right: &TreeNode{
+								Val: 3,
+								Left: &TreeNode{
+									Val: 3,
+									Left: &TreeNode{
+										Val: 3,
+										Left: &TreeNode{
+											Val:   3,
+											Left:  nil,
+											Right: nil,
+										},
+										Right: &TreeNode{
+											Val:  -2,
+											Left: nil,
+											Right: &TreeNode{
+												Val: 3,
+												Left: &TreeNode{
+													Val:   3,
+													Left:  nil,
+													Right: nil,
+												},
+												Right: &TreeNode{
+													Val:   -2,
+													Left:  nil,
+													Right: nil,
+												},
+											},
+										},
+									},
+									Right: nil,
+								},
+								Right: &TreeNode{
+									Val:   -2,
+									Left:  nil,
+									Right: nil,
+								},
+							},
+						},
+					},
+					Right: &TreeNode{
+						Val: 3,
+						Left: &TreeNode{
+							Val:   3,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: &TreeNode{
+							Val: -2,
+							Left: &TreeNode{
+								Val: 3,
+								Left: &TreeNode{
+									Val:   3,
+									Left:  nil,
+									Right: nil,
+								},
+								Right: &TreeNode{
+									Val:   -2,
+									Left:  nil,
+									Right: nil,
+								},
+							},
+							Right: nil,
+						},
+					},
+				},
+			},
+			Right: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val:  -2,
+						Left: nil,
+						Right: &TreeNode{
+							Val: 3,
+							Left: &TreeNode{
+								Val:  3,
+								Left: nil,
+								Right: &TreeNode{
+									Val: 3,
+									Left: &TreeNode{
+										Val:   3,
+										Left:  nil,
+										Right: nil,
+									},
+									Right: &TreeNode{
+										Val: -2,
+										Left: &TreeNode{
+											Val: 3,
+											Left: &TreeNode{
+												Val:   3,
+												Left:  nil,
+												Right: nil,
+											},
+											Right: &TreeNode{
+												Val:  -2,
+												Left: nil,
+												Right: &TreeNode{
+													Val: 3,
+													Left: &TreeNode{
+														Val:  3,
+														Left: nil,
+														Right: &TreeNode{
+															Val: 3,
+															Left: &TreeNode{
+																Val:   3,
+																Left:  nil,
+																Right: nil,
+															},
+															Right: &TreeNode{
+																Val:  -2,
+																Left: nil,
+																Right: &TreeNode{
+																	Val: 3,
+																	Left: &TreeNode{
+																		Val:   3,
+																		Left:  nil,
+																		Right: nil,
+																	},
+																	Right: &TreeNode{
+																		Val:  -2,
+																		Left: nil,
+																		Right: &TreeNode{
+																			Val: 3,
+																			Left: &TreeNode{
+																				Val:  3,
+																				Left: nil,
+																				Right: &TreeNode{
+																					Val: 3,
+																					Left: &TreeNode{
+																						Val: 3,
+																						Left: &TreeNode{
+																							Val: 3,
+																							Left: &TreeNode{
+																								Val:   3,
+																								Left:  nil,
+																								Right: nil,
+																							},
+																							Right: &TreeNode{
+																								Val:  -2,
+																								Left: nil,
+																								Right: &TreeNode{
+																									Val: 3,
+																									Left: &TreeNode{
+																										Val:  3,
+																										Left: nil,
+																										Right: &TreeNode{
+																											Val: 3,
+																											Left: &TreeNode{
+																												Val:   3,
+																												Left:  nil,
+																												Right: nil,
+																											},
+																											Right: &TreeNode{
+																												Val:   -2,
+																												Left:  nil,
+																												Right: nil,
+																											},
+																										},
+																									},
+																									Right: &TreeNode{
+																										Val:   -2,
+																										Left:  nil,
+																										Right: nil,
+																									},
+																								},
+																							},
+																						},
+																						Right: nil,
+																					},
+																					Right: &TreeNode{
+																						Val:   -2,
+																						Left:  nil,
+																						Right: nil,
+																					},
+																				},
+																			},
+																			Right: &TreeNode{
+																				Val:   -2,
+																				Left:  nil,
+																				Right: nil,
+																			},
+																		},
+																	},
+																},
+															},
+														},
+													},
+													Right: &TreeNode{
+														Val:   -2,
+														Left:  nil,
+														Right: nil,
+													},
+												},
+											},
+										},
+										Right: nil,
+									},
+								},
+							},
+							Right: &TreeNode{
+								Val:   -2,
+								Left:  nil,
+								Right: nil,
+							},
+						},
+					},
+				},
+				Right: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val: 3,
+						Left: &TreeNode{
+							Val:   3,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: &TreeNode{
+							Val:  -2,
+							Left: nil,
+							Right: &TreeNode{
+								Val: 3,
+								Left: &TreeNode{
+									Val: 3,
+									Left: &TreeNode{
+										Val: 3,
+										Left: &TreeNode{
+											Val:   3,
+											Left:  nil,
+											Right: nil,
+										},
+										Right: &TreeNode{
+											Val: -2,
+											Left: &TreeNode{
+												Val: 3,
+												Left: &TreeNode{
+													Val:   3,
+													Left:  nil,
+													Right: nil,
+												},
+												Right: &TreeNode{
+													Val:   -2,
+													Left:  nil,
+													Right: nil,
+												},
+											},
+											Right: nil,
+										},
+									},
+									Right: nil,
+								},
+								Right: &TreeNode{
+									Val:   -2,
+									Left:  nil,
+									Right: nil,
+								},
+							},
+						},
+					},
+					Right: &TreeNode{
+						Val: 3,
+						Left: &TreeNode{
+							Val:   3,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: &TreeNode{
+							Val:   -2,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val:  -3,
+			Left: nil,
+			Right: &TreeNode{
+				Val:   11,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+	}
+
+	actualResult := pathSum(tree, 8)
+	expectedResult := 37
+	if expectedResult != actualResult {
+		t.Errorf("The expected result should be %d, but we got %d", expectedResult, actualResult)
+	}
 }
+
+// func makeTree(numbers []int) {
+//     for _, number := range numbers {
+//
+//     }
+// }
